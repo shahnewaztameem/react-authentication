1d2bf33ab100f4e6b2a94cc349e21f576934eb1e
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-08-01 16:46:29 +0600

    implemented login route, controller and send back JWT response

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,3 +1,4 @@
+import { loginRoute } from './loginRoute'
 import { signUpRoute } from './signUpRoute'
 import { testRoute } from './testRoute'
-export const routes = [testRoute, signUpRoute]
+export const routes = [testRoute, signUpRoute, loginRoute]
